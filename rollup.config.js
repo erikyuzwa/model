e71999ddb40de9acbd0baca121277746b598945b
erikyuzwa <erikyuzwa@gmail.com>
2024-11-23 14:06:59 -0700

    feat: updated with rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'index.js',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs()
+  ],
+  external: [],
+}
